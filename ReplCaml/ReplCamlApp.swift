ffc72062ee3d671e139d2f5d5368226daa37cccb
miche <miche@mac.com>
2025-06-19 11:45:48 +0900

    Initial Commit

---
+++ ReplCaml/ReplCamlApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReplCamlApp.swift
+//  ReplCaml
+//
+//  Created by Mitsuhiro Kondo on 2025/06/19.
+//
+
+import SwiftUI
+
+@main
+struct ReplCamlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
