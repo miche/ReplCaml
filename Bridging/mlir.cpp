b07b5ee6a3311d32bdcd72248e2fa0659ba751d7
miche <miche@mac.com>
2025-07-25 09:57:46 +0900

    deleted OpBuilder parameter TODO: consider memory leaks

--- Bridging/mlir.cpp
+++ Bridging/mlir.cpp
@@ -28,8 +28,6 @@
 #include <mlir/Target/LLVMIR/ModuleTranslation.h>
 #include <mlir/Target/LLVMIR/LLVMTranslationInterface.h>
 //#include "mlir/Target/LLVMIR/Export.h"
-//#include "llvm/IR/Module.h"  // TODO:
-//#include "llvm/Support/TargetSelect.h"  // TODO:
 #include <memory>
 
 class MLIRKit::Impl {
@@ -44,34 +42,7 @@ public:
     mlir::ComplexType complex_t;
 
     inline mlir::Location loc() {
-        return loc(*modBuilder);
-    }
-    inline mlir::Location loc(mlir::OpBuilder &bldr) {
-        return bldr.getUnknownLoc();
-    }
-    inline mlir::TypedValue<mlir::Type> i32_v(mlir::arith::ConstantOp op) {
-        return op.getResult();
-    }
-    inline mlir::TypedValue<mlir::Type> i32(mlir::OpBuilder &bldr, int32_t value) {
-        return i32_v(i32_op(bldr, value));
-    }
-    inline mlir::TypedValue<mlir::Type> f32_v(mlir::arith::ConstantOp op) {
-        return op.getResult();
-    }
-    inline mlir::TypedValue<mlir::Type> f32(mlir::OpBuilder &bldr, float value) {
-        return f32_v(f32_op(bldr, value));
-    }
-
-    inline mlir::TypedValue<mlir::Type> i32(int32_t value) {
-        return i32_v(i32_op(value));
-    }
-
-
-    inline mlir::func::FuncOp lookup(mlir::StringRef name) {
-        return mod->lookupSymbol<mlir::func::FuncOp>(name);
-    }
-    inline mlir::FunctionType getFunctionType(mlir::func::FuncOp fn) {
-        return fn.getFunctionType();
+        return modBuilder->getUnknownLoc();
     }
 
     // llvm dialect
@@ -81,81 +52,96 @@ public:
     inline mlir::Type llvm_ptr_t(void) {
         return mlir::LLVM::LLVMPointerType::get(context.get());
     }
-    // llvm.mlir.constant
-    inline mlir::LLVM::ConstantOp llvm_constant(mlir::OpBuilder &bldr, mlir::Type typ, int64_t value) {
-        return bldr.create<mlir::LLVM::ConstantOp>(loc(), typ, value);
+    inline mlir::LLVM::ConstantOp llvm_constant(mlir::Type typ, int64_t value) {
+        return funcBuilder->create<mlir::LLVM::ConstantOp>(loc(), typ, value);  // llvm.mlir.constant
     }
-    inline mlir::LLVM::AllocaOp llvm_alloca(mlir::OpBuilder &bldr, mlir::TypeAttr t, mlir::Value n) {
-        return bldr.create<mlir::LLVM::AllocaOp>(loc(), llvm_ptr_t(), n, bldr.getI64IntegerAttr(4), t);
+    inline mlir::LLVM::AllocaOp llvm_alloca(mlir::TypeAttr t, mlir::Value n) {
+        return funcBuilder->create<mlir::LLVM::AllocaOp>(loc(), llvm_ptr_t(), n, funcBuilder->getI64IntegerAttr(4), t);
     }
-    inline mlir::LLVM::StoreOp llvm_store(mlir::OpBuilder &bldr, mlir::Value value, mlir::Value ref) {
-        return bldr.create<mlir::LLVM::StoreOp>(loc(), value, ref, 4);
+    inline mlir::LLVM::StoreOp llvm_store(mlir::Value value, mlir::Value ref) {
+        return funcBuilder->create<mlir::LLVM::StoreOp>(loc(), value, ref, 4);
     }
-    inline mlir::LLVM::LoadOp llvm_load(mlir::OpBuilder &bldr, mlir::Type t, mlir::Value ref) {
-        return bldr.create<mlir::LLVM::LoadOp>(loc(), t, ref, 4);
+    inline mlir::LLVM::LoadOp llvm_load(mlir::Type t, mlir::Value ref) {
+        return funcBuilder->create<mlir::LLVM::LoadOp>(loc(), t, ref, 4);
     }
 
     // memref dialect
-    inline mlir::memref::AllocaOp memref_alloca(mlir::OpBuilder &bldr, mlir::MemRefType typ) {
-        return bldr.create<mlir::memref::AllocaOp>(loc(), typ);
+    inline mlir::memref::AllocaOp memref_alloca(mlir::MemRefType typ) {
+        return funcBuilder->create<mlir::memref::AllocaOp>(loc(), typ);
     }
-    inline mlir::memref::StoreOp memref_store(mlir::OpBuilder &bldr, mlir::Value value, mlir::Value memref, mlir::ValueRange indices) {
-        return bldr.create<mlir::memref::StoreOp>(loc(), value, memref, indices);
+    inline mlir::memref::StoreOp memref_store(mlir::Value value, mlir::Value memref, mlir::ValueRange indices) {
+        return funcBuilder->create<mlir::memref::StoreOp>(loc(), value, memref, indices);
     }
-    inline mlir::memref::LoadOp memref_load(mlir::OpBuilder &bldr, mlir::Value memref, mlir::ValueRange indices) {
-        return bldr.create<mlir::memref::LoadOp>(loc(), memref, indices);
+    inline mlir::memref::LoadOp memref_load(mlir::Value memref, mlir::ValueRange indices) {
+        return funcBuilder->create<mlir::memref::LoadOp>(loc(), memref, indices);
     }
 
     // arith dialect
-    inline mlir::arith::AddIOp arith_addi(mlir::OpBuilder &bldr, mlir::Value lhs, mlir::Value rhs) {
-        return bldr.create<mlir::arith::AddIOp>(loc(), lhs, rhs);
-    }
-    inline mlir::arith::ConstantOp i32_op(mlir::OpBuilder &bldr, int32_t value) {
-        return bldr.create<mlir::arith::ConstantIntOp>(loc(bldr), value, i32_t);
-    }
-    inline mlir::arith::ConstantOp f32_op(mlir::OpBuilder &bldr, mlir::FloatAttr attr) {
-        return bldr.create<mlir::arith::ConstantOp>(loc(bldr), attr);
-    }
-    inline mlir::arith::ConstantOp f32_op(mlir::OpBuilder &bldr, float value) {
-        return f32_op(bldr, bldr.getF32FloatAttr(value));
-    }
-
     inline mlir::arith::AddIOp arith_addi(mlir::Value lhs, mlir::Value rhs) {
         return funcBuilder->create<mlir::arith::AddIOp>(loc(), lhs, rhs);
     }
-    inline mlir::arith::ConstantOp i32_op(int32_t value) {
+    inline mlir::arith::ConstantOp arith_constant_i32_op(int32_t value) {
         return funcBuilder->create<mlir::arith::ConstantIntOp>(loc(), value, i32_t);
+    }
+    inline mlir::arith::ConstantOp arith_constant_f32_op(mlir::FloatAttr attr) {
+        return funcBuilder->create<mlir::arith::ConstantOp>(loc(), attr);
+    }
+    inline mlir::arith::ConstantOp arith_constant_f32_op(float value) {
+        return arith_constant_f32_op(funcBuilder->getF32FloatAttr(value));
+    }
+    inline mlir::TypedValue<mlir::Type> arith_constant_v(mlir::arith::ConstantOp op) {
+        return op.getResult();
+    }
+    inline mlir::TypedValue<mlir::Type> arith_constant_f32(float value) {
+        return arith_constant_v(arith_constant_f32_op(value));
+    }
+    inline mlir::TypedValue<mlir::Type> arith_constant_i32(int32_t value) {
+        return arith_constant_v(arith_constant_i32_op(value));
+    }
+    inline mlir::arith::ConstantOp arith_constant_index(int32_t value) {
+        return funcBuilder->create<mlir::arith::ConstantIndexOp>(loc(), value);
     }
 
     // complex dialect
-    inline mlir::complex::CreateOp complex(mlir::OpBuilder &bldr, float real, float image) {
-        return bldr.create<mlir::complex::CreateOp>(loc(bldr), complex_t, f32_op(bldr, real), f32_op(bldr, image));
+    inline mlir::complex::CreateOp complex(float real, float image) {
+        return funcBuilder->create<mlir::complex::CreateOp>(loc(), complex_t, arith_constant_f32_op(real), arith_constant_f32_op(image));
     }
-    inline mlir::complex::MulOp complex_mul(mlir::OpBuilder &bldr, mlir::Value lhs, mlir::Value rhs) {
-        return bldr.create<mlir::complex::MulOp>(loc(), complex_t, lhs, rhs);
+    inline mlir::complex::MulOp complex_mul(mlir::Value lhs, mlir::Value rhs) {
+        return funcBuilder->create<mlir::complex::MulOp>(loc(), complex_t, lhs, rhs);
     }
-    inline mlir::complex::ConjOp complex_conj(mlir::OpBuilder &bldr, mlir::Value rhs) {
-        return bldr.create<mlir::complex::ConjOp>(loc(), complex_t, rhs);
+    inline mlir::complex::ConjOp complex_conj(mlir::Value rhs) {
+        return funcBuilder->create<mlir::complex::ConjOp>(loc(), complex_t, rhs);
     }
 
     // func dialect
-    inline mlir::func::CallIndirectOp func_call_indirect(mlir::OpBuilder &bldr, mlir::FunctionType ft, mlir::func::ConstantOp fc, mlir::ValueRange arg) {
-        return bldr.create<mlir::func::CallIndirectOp>(loc(), ft.getResults(), fc.getResult(), arg); // func.call_indirect
+    inline mlir::func::CallIndirectOp func_call_indirect(mlir::FunctionType ft, mlir::func::ConstantOp fc, mlir::ValueRange arg) {
+        return funcBuilder->create<mlir::func::CallIndirectOp>(loc(), ft.getResults(), fc.getResult(), arg); // func.call_indirect
     }
-    inline mlir::func::CallOp func_call(mlir::OpBuilder &bldr, mlir::func::FuncOp fn, mlir::ValueRange args) {
-        return bldr.create<mlir::func::CallOp>(loc(), fn, args); // func.call
+    inline mlir::func::CallOp func_call(mlir::func::FuncOp fn, mlir::ValueRange args) {
+        return funcBuilder->create<mlir::func::CallOp>(loc(), fn, args); // func.call
     }
-    inline mlir::func::ConstantOp func_constant(mlir::OpBuilder &bldr, mlir::FunctionType ft, mlir::StringRef name) {
-        return bldr.create<mlir::func::ConstantOp>(loc(), ft, name); // func.constant
+    inline mlir::func::ConstantOp func_constant(mlir::FunctionType ft, mlir::StringRef name) {
+        return funcBuilder->create<mlir::func::ConstantOp>(loc(), ft, name); // func.constant
     }
-    inline mlir::func::FuncOp func_func(mlir::OpBuilder &bldr, mlir::StringRef name, mlir::FunctionType ft) {
-        return bldr.create<mlir::func::FuncOp>(loc(), name, ft);    // func.func
+    inline mlir::func::FuncOp func_func(mlir::StringRef name, mlir::TypeRange arg, mlir::TypeRange ret) {
+        mlir::FunctionType funcType = modBuilder->getFunctionType(arg, ret);
+        mlir::func::FuncOp func = modBuilder->create<mlir::func::FuncOp>(loc(), name, funcType);    // func.func
+        entryBlock = func.addEntryBlock(); // TODO: memory may leak
+        mlir::OpBuilder t(entryBlock, entryBlock->begin());
+        funcBuilder = std::make_shared<mlir::OpBuilder>(t);
+        return func;
     }
-    inline mlir::func::ReturnOp func_ret(mlir::OpBuilder &bldr, mlir::TypedValue<mlir::Type> value) {
-        return bldr.create<mlir::func::ReturnOp>(loc(), value);    // func.ret
+    inline mlir::BlockArgument func_arg(unsigned int index) {
+        return entryBlock->getArgument(index);
     }
     inline mlir::func::ReturnOp func_ret(mlir::TypedValue<mlir::Type> value) {
         return funcBuilder->create<mlir::func::ReturnOp>(loc(), value);    // func.ret
+    }
+    inline mlir::func::FuncOp func_lookup(mlir::StringRef name) {
+        return mod->lookupSymbol<mlir::func::FuncOp>(name);
+    }
+    inline mlir::FunctionType getFunctionType(mlir::func::FuncOp fn) {
+        return fn.getFunctionType();
     }
 
 private:
@@ -164,6 +150,7 @@ private:
     std::unique_ptr<mlir::ModuleOp> mod;
     std::unique_ptr<mlir::OpBuilder> modBuilder;
     std::shared_ptr<mlir::OpBuilder> funcBuilder;
+    mlir::Block *entryBlock;
 };
 
 MLIRKit::MLIRKit() : impl(new Impl()) {}
@@ -200,56 +187,48 @@ void MLIRKit::Impl::generateIR() {
     modBuilder->setInsertionPointToStart(mod->getBody());
 
     if (true) {
-        mlir::FunctionType funcType = modBuilder->getFunctionType({i32_t}, {i32_t});
-        mlir::func::FuncOp func = func_func(*modBuilder, "sample_function", funcType);
+        mlir::func::FuncOp func = func_func("sample_function", {i32_t}, {i32_t});
         func.setPrivate();
-        mlir::Block &entryBlock = *func.addEntryBlock();
-        mlir::OpBuilder t(&entryBlock, entryBlock.begin());
-        funcBuilder = std::make_shared<mlir::OpBuilder>(t);
-        mlir::BlockArgument arg = entryBlock.getArgument(0);
-//        mlir::arith::AddIOp addOp = arith_addi(funcBuilder, arg, i32(funcBuilder, 42));
-//        func_ret(funcBuilder, addOp.getResult());
-        mlir::arith::AddIOp addOp = arith_addi(arg, i32(42));
+        mlir::BlockArgument arg = func_arg(0);
+        mlir::arith::AddIOp addOp = arith_addi(arg, arith_constant_i32(42));
         func_ret(addOp.getResult());
     }
     if (true) {
-        mlir::FunctionType cft = modBuilder->getFunctionType({complex_t, complex_t}, {complex_t});
-        mlir::func::FuncOp cmul = modBuilder->create<mlir::func::FuncOp>(loc(), "complex_mul", cft);
+        mlir::func::FuncOp cmul = func_func("complex_mul", {complex_t, complex_t}, {complex_t});
         cmul.setPrivate();
-        mlir::Block &entryBlock = *cmul.addEntryBlock();
-        mlir::OpBuilder funcBuilder(&entryBlock, entryBlock.begin());
-        mlir::BlockArgument lhs = entryBlock.getArgument(0);
-        mlir::BlockArgument rhs = entryBlock.getArgument(1);
-        auto mulOp = complex_mul(funcBuilder, lhs, rhs);
-        func_ret(funcBuilder, mulOp.getResult());
+        mlir::BlockArgument lhs = func_arg(0);
+        mlir::BlockArgument rhs = func_arg(1);
+        mlir::complex::MulOp mulOp = complex_mul(lhs, rhs);
+        func_ret(mulOp.getResult());
     }
     if (true) {
-        auto funcType = modBuilder->getFunctionType({}, {i32_t});
-        auto func = modBuilder->create<mlir::func::FuncOp>(loc(), "main", funcType);
-        auto &entryBlock = *func.addEntryBlock();
-        mlir::OpBuilder funcBuilder(&entryBlock, entryBlock.begin());
-        //builder->setInsertionPoint(&entryBlock, entryBlock.begin());
-
-//        auto pos = memref_alloca(funcBuilder, mlir::MemRefType::get({1}, i32_t));
-//        mlir::Value index = funcBuilder.create<mlir::arith::ConstantIndexOp>(loc(), 0);
-//        memref_store(funcBuilder, i32(funcBuilder, 123), pos, index);
-//        memref_load(funcBuilder, pos, index);
-
-        auto i1 = llvm_constant(funcBuilder, i32_t, 1);
-        auto i0 = llvm_constant(funcBuilder, i32_t, 0);
-        auto a = llvm_alloca(funcBuilder, mlir::TypeAttr::get(i32_t), i1.getResult());
-        llvm_store(funcBuilder, i1, a);
-        auto b = llvm_load(funcBuilder, i32_t, a);
-
-        auto cmul = lookup("complex_mul");
-        auto v = complex(funcBuilder, 3.0, 4.0);
-        func_call(funcBuilder, cmul, {v, v});
-
+        mlir::func::FuncOp func = func_func("memref_sample", {}, {i32_t});
+        mlir::memref::AllocaOp pos = memref_alloca(mlir::MemRefType::get({1}, i32_t));
+        mlir::Value index = arith_constant_index(0);
+        memref_store(arith_constant_i32(123), pos, index);
+        memref_load(pos, index);
+        func_ret(arith_constant_i32(0));
+    }
+    if (true) {
+        mlir::func::FuncOp func = func_func("complex_sample", {}, {i32_t});
+        auto v = complex(3.0, 4.0);
+        mlir::func::FuncOp cmul = func_lookup("complex_mul");
+        func_call(cmul, {v, v});
         mlir::FunctionType cft = cmul.getFunctionType();
-        mlir::func::ConstantOp fc = func_constant(funcBuilder, cft, "complex_mul");
-        func_call_indirect(funcBuilder, cft, fc, {v, v});
+        mlir::func::ConstantOp fc = func_constant(cft, "complex_mul");
+        func_call_indirect(cft, fc, {v, v});
 
-        func_ret(funcBuilder, i32(funcBuilder, 0));
+        func_ret(arith_constant_i32(0));
+    }
+    if (true) {
+        mlir::func::FuncOp func = func_func("main", {}, {i32_t});
+        auto i1 = llvm_constant(i32_t, 1);
+        auto i0 = llvm_constant(i32_t, 0);
+        auto a = llvm_alloca(mlir::TypeAttr::get(i32_t), i1.getResult());
+        llvm_store(i1, a);
+        auto b = llvm_load(i32_t, a);
+
+        func_ret(arith_constant_i32(0));
     }
 
     mod->print(llvm::outs());
